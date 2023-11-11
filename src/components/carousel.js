c628d50a5954bf72c07d400542eeebef03084cc8
zinnia <zinniazia.99@gmail.com>
2023-11-12 00:56:19 +0500

    faizan

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,28 +3,33 @@ import { useTheme } from "@mui/material/styles";
 import Carousel from "react-bootstrap/Carousel";
 import ExampleCarouselImage from "../images/BG-Working-With-Entrepreneurs.png";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { Container , Grid} from "@mui/material";
-import "../styles/HomeStyles.css";
+import { Container, Grid } from "@mui/material";
+// import "../styles/HomeStyles.css";
+import CEO from "../images/CEO.jpg";
+import CO from "../images/career1.jpg";
+import HR from "../images/HR.jpg";
+
 const images = [
   {
     label: "Hash Technologies",
     text: "We, at Hash Technologies, provide you world-class and highly satisfactory services across various verticals in IT-Services & HR Services.",
-    name: "Scott Hanks",
-    position: "Chief Officer",
-    imgPath:
-      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+    name: "James Andrew",
+    position: "CEO",
+    imgPath: CEO,
   },
   {
     label: "Hash Technologies",
     text: "In IT-Services we provide, Software & Hardware Solutions, DevSecOps, Digital Marketing, Web Development, Cloud Solutions, Data Management & Analytics, Machine Learning & AI, business technologies, consulting services, implementation services, advisory services and managed services.",
-    imgPath:
-      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+    name: "Saim Ali",
+    position: "Co-Founder",
+    imgPath: CO,
   },
   {
     label: "Hash Technologies",
     text: "In HR-Services we Provide, Professional Staffing, Talent Acquisition, Resource outsourcing, Project management, Leadership Development & Training/Coaching.",
-    imgPath:
-      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+    name: "Faraz Toor",
+    position: "HR",
+    imgPath: HR,
   },
 ];
 
@@ -47,21 +52,18 @@ const CarouselBox = () => {
             <Carousel.Item className="">
               <Grid container spacing={6}>
                 <Grid item xs={12} sm={6}>
-                <img
-                  className="w-100"
-                  style={{ height: "60vh" }}
-                  src={item.imgPath}
-                  text="First slide"
-                />
-                {/* <Carousel.Caption>
-                <p>{item.label}</p>
-              </Carousel.Caption> */}
+                  <img
+                    className="w-100"
+                    style={{ height: "60vh" }}
+                    src={item.imgPath}
+                    text="First slide"
+                  />
                 </Grid>
-                <Grid item xs={12} sm={6} p={15}>
-                <h2 class="text-dark">{item.label}</h2>
-                <p class="p-5 text-justify">"{item.text}"</p>
-                <h5 class="text-left text-dark">{item.name}</h5>
-                <p class="text-left">{item.position}</p>
+                <Grid item xs={12} sm={6} p={10} sx={{height:"500px"}}>
+                  <h2 class="text-dark">{item.label}</h2>
+                  <p class="p-5 text-justify">"{item.text}"</p>
+                  <h5 class="text-left text-dark">{item.name}</h5>
+                  <p class="text-left">{item.position}</p>
                 </Grid>
               </Grid>
             </Carousel.Item>
